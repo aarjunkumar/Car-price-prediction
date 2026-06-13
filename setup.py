e572a79f71bfb999d9cd1fb80570229419f49e49
aarrjunkkumar <aarrjunjkkumar619@gmail.com>
2026-06-13 11:40:02 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT="-e ."
+def get_packages(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file:
+        requirements=file.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="car price prediction",
+    version="0.0.1",
+    author="Arjun Kumar",
+    author_email="aarrjunkkumar619@gmail.com",
+    packages=find_packages(),
+    install_requires=get_packages("requirements.txt")
+)
