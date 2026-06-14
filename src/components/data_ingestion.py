39f5a813484969715bc58c111ac2d28e2a494ccf
aarrjunkkumar <aarrjunjkkumar619@gmail.com>
2026-06-14 10:13:14 +0530

    Data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,13 @@
+import sys
 import os 
-import sys 
-import pandas as pd 
+import pandas as pd
+from dataclasses import dataclass
 
 from sklearn.model_selection import train_test_split
 
 from src.exception import CustomException
 from src.logger import logging
-from dataclasses import dataclass
+
 
 @dataclass
 class DataIngestionConfig:
@@ -22,9 +23,9 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv("notebook/data/car_price_prediction.csv")
-            logging.info("Read the dataset as dataframe")
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
@@ -32,15 +33,16 @@ class DataIngestion:
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            train_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the dats is completed")
+            logging.info("Inmgestion of the data iss completed")
+
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
-        
-        except Exception as e:
+
+        except Exception as e: 
             raise CustomException(e,sys)
 
 if __name__=="__main__":
